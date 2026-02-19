5c4158b82674e5307574bb688007b6b211489bee
AlineCoatanoan <aline.coatanoan@gmail.com>
2026-02-19 12:09:59 +0100

    file writer/front : update config playwright

--- wm-rajar-ms_writer/FRONT/playwright.config.ts
+++ wm-rajar-ms_writer/FRONT/playwright.config.ts
@@ -12,7 +12,7 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './tests',
+  testDir: 'E2E/tests',
   // Durée maximale par test (ms)
   timeout: 45000,
   // Timeout par défaut pour les assertions expect
@@ -32,7 +32,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: 'http://localhost:5173',
+    baseURL: 'http://localhost:5174',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -86,11 +86,6 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  webServer: {
-    command: 'pnpm run dev',
-    url: 'http://localhost:5173',
-    reuseExistingServer: !process.env.CI,
-    cwd: '../FRONT',
-    timeout: 120000,
-  },
+  /* webServer : deleted because we used docker*/
+
 });
