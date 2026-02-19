87d80851244553410424561af6d58c82c6c2b125
AlineCoatanoan <aline.coatanoan@gmail.com>
2026-02-19 12:18:44 +0100

    file writer/front: Disable the CI E2E options (forbidOnly, retries, workers) because there is no E2E execution in the CI pipeline at the moment, and no types/nodes are installed in the front end.

--- wm-rajar-ms_writer/FRONT/playwright.config.ts
+++ wm-rajar-ms_writer/FRONT/playwright.config.ts
@@ -21,12 +21,17 @@ export default defineConfig({
   },
   /* Run tests in files in parallel */
   fullyParallel: true,
+
+  //partie commentée pour éviter les erreurs car pas de types/node installé pour le moment. 
+  //à réactiver si les tests e2e sont exécutés dans le pipeline CI.
+
   /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
+    //forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+    //retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+    //workers: process.env.CI ? 1 : undefined,
+
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
