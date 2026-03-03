64d540e2fb4a7314a62c1dfc2678f5152f50131a
AlineCoatanoan <aline.coatanoan@gmail.com>
2026-03-03 14:49:11 +0100

    reader/backend: test unit: add unit test for validate middleware

---
+++ wn-rajar-ms_reader/Backend/src/tests/unit/validate.test.js
@@ -0,0 +1,34 @@
+const validate = require('../../middlewares/validate');
+const { getArticleByIdSchema } = require('../../schemas/articles.schema');
+
+// le middleware qui valide les données d'une requête avant qu'elles arrivent au controller.
+// ici on veut tester l'id
+
+describe('validate middleware', () => {
+
+    test('id validate', () => {
+        const validateId = validate(getArticleByIdSchema, 'params');
+
+        const req = { params: { id: '1' } };
+        const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
+        const next = jest.fn();
+
+        validateId(req, res, next);
+
+        expect(next).toHaveBeenCalled();
+        expect(req.params).toEqual({ id: 1 });
+    });
+
+    test('id validate error', () => {
+        const validateId = validate(getArticleByIdSchema, 'params');
+
+        const req = { params: { id: 'jecrisnimportequoi' } };
+        const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
+        const next = jest.fn();
+
+        validateId(req, res, next);
+
+        expect(next).not.toHaveBeenCalled();
+        expect(res.status).toHaveBeenCalledWith(400);
+    });
+});
