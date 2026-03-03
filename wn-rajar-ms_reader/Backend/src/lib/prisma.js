cc86d88fea8a5142f2f61e1378f769c2471e1d3d
AlineCoatanoan <aline.coatanoan@gmail.com>
2026-03-03 11:23:27 +0100

    config: bdd test

--- wn-rajar-ms_reader/Backend/src/lib/prisma.js
+++ wn-rajar-ms_reader/Backend/src/lib/prisma.js
@@ -1,5 +1,13 @@
 const { PrismaClient } = require('@prisma/client');
 const { PrismaPg } = require('@prisma/adapter-pg');
+const dotenv = require('dotenv');
+
+// Chargement du bon .env selon l'environnement
+if (process.env.NODE_ENV === 'test') {
+  dotenv.config({ path: '.env.test' });
+} else {
+  dotenv.config();
+}
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 const prisma = new PrismaClient({ adapter });
