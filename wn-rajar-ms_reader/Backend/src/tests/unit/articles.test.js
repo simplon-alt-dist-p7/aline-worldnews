6eefce10211cd70570206c9e3f35237e4e0ed8a5
AlineCoatanoan <aline.coatanoan@gmail.com>
2026-03-16 13:47:40 +0100

    reader: unit test: articles.test.js (return article ID & not found)

---
+++ wn-rajar-ms_reader/Backend/src/tests/unit/articles.test.js
@@ -0,0 +1,31 @@
+const ArticlesService = require('../../services/articles');
+const prisma = require('../../lib/prisma');
+
+//Contrairement à comments.test.js ici on récupère lib/prisma car on a besoin de méthodes différentes 
+// dans le mock. Dans comments le mock était statique. 
+
+jest.mock('../../lib/prisma', () => ({
+  article: {
+    findFirst: jest.fn().mockResolvedValue({ id: 1, title: 'Article 1', body: 'Contenu de l\'article 1' }),
+  },
+}));
+
+describe('ArticlesService', () => {
+  let articlesService;
+
+  beforeEach(() => {
+    articlesService = new ArticlesService();
+  });
+
+  test('Return article', async () => {
+    const result = await articlesService.getById(1);
+    expect(result).toHaveProperty('id'); 
+    expect(result.body).toBe('Contenu de l\'article 1');
+  });
+
+  test('Article not found', async () => {
+  prisma.article.findFirst.mockResolvedValueOnce(null); //ici on modifie le comportement du mock en null. 
+  const result = await articlesService.getById(999);
+  expect(result).toBeNull();
+  });
+});
