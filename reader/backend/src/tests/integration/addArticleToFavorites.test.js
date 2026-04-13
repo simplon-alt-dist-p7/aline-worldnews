56517836cec7cd96c8a9e3fd3089df895bd87e6f
AlineCoatanoan <aline.coatanoan@gmail.com>
2026-04-13 10:13:50 +0200

    reader/backend: test/integration/addArticleToFavorites

---
+++ reader/backend/src/tests/integration/addArticleToFavorites.test.js
@@ -0,0 +1,25 @@
+const request = require('supertest');
+const app = require('../../index');
+const prisma = require('../../prisma');
+
+describe('/articles/:id/favorite', () => {
+  afterEach(async () => {
+    await prisma.articleFavorite.deleteMany();
+  });
+
+  test.each([['deslettres'], ['-5']])('return 400 for invalid id', async (invalidId) => {
+    const response = await request(app).post(`/articles/${invalidId}/favorite`);
+    expect(response.status).toBe(400);
+    expect(response.body).toHaveProperty('error', 'Erreur de validation');
+  });
+
+  test('ajouter un article aux favoris', async () => {
+    //on récupère un article Id valide
+    const articlesResponse = await request(app).get('/articles');
+    const articleId = articlesResponse.body.articles[0].id;
+
+    //on ajoute cet article aux favoris
+    const response = await request(app).post(`/articles/${articleId}/favorite`);
+    expect(response.status).toBe(201);
+  });
+});
